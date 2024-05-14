267371c232d72be44e07c8aba21f7b74819ca234
LSZ2001 <liu2012556104@gmail.com>
2024-05-14 12:17:09 -0400

    5-15-2024 version

--- manuscript_allplots.m
+++ manuscript_allplots.m
@@ -188,6 +188,7 @@ causal_inf_strategy = "ProbMatching";
 save_name = "PM";
 manuscript_allfits_respdistrvisualization_semiparaminsp_resc(causal_inf_strategy, fontsize, figsize_RespDistr, figpath, save_name, png_dpi, model_path, plot_lapse, lapse_type);
 
+%%
 causal_inf_strategy = "ModelSelection";
 save_name = "MS";
 manuscript_allfits_respdistrvisualization_semiparaminsp_resc(causal_inf_strategy, fontsize, figsize_RespDistr, figpath, save_name, png_dpi, model_path, plot_lapse, lapse_type);
